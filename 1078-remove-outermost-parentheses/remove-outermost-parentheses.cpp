6779777814381d6bbe419445f2e213dedbac97d7
Devanshsingh25 <nikk501101@gmail.com>
2026-07-27 22:14:30 +0530

    Time: 22 ms (5.65%) | Memory: 69.9 MB (5.47%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        vector<string>v;
+        string result="";
+        int n = s.size();
+        int count=0;
+       for(int i = 0;i<n;i++){
+          if(s[i]=='('){
+            count++;
+            if(count>1){
+              v.push_back("(");
+            }
+          }
+          else if(s[i]==')'){
+            count--;
+            if(count>0){
+                v.push_back(")");
+            }
+          }
+       }
+       for(int i=0;i<v.size();i++){
+          result = result+v[i];
+       }
+       return result;
+
+        
+    }
+};
