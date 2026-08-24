2f5ca7ec7e90d2caf1f6d246200968877d561580
Devanshsingh25 <nikk501101@gmail.com>
2026-08-24 10:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (99.86%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        string res = s+t;
-         char ch = res[0];
-        for(int i =1;i<(int)res.size();i++){
-           ch = ch^res[i];
-        }
-       return ch;
+    //     string res = s+t;
+    //      char ch = res[0];
+    //     for(int i =1;i<(int)res.size();i++){
+    //        ch = ch^res[i];
+    //     }
+    //    return ch;
+          
+          for(int i =0;i<s.size();i++){
+            t[i+1]= t[i+1]+t[i]-s[i];
+          }
+          return t[t.size()-1];
         
     }
 };
