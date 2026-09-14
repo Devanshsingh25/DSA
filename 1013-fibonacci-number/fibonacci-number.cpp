e5e7e5716b35a8c4a1f02d8b4c08fbfbbe9e0417
Devanshsingh25 <nikk501101@gmail.com>
2026-09-14 16:04:53 +0530

    Time: 3 ms (64.47%) | Memory: 8.4 MB (11.33%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-    int fibonacci(int n,vector<int>&dp){
-        if(n==0)
-        return 0;
-        if(n==1)
-        return 1;
-        if(dp[n]!=-1){
-            return dp[n];
-        }
+    // int fibonacci(int n,vector<int>&dp){
+    //     if(n==0)
+    //     return 0;
+    //     if(n==1)
+    //     return 1;
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
 
-        dp[n]= fibonacci(n-1,dp)+fibonacci(n-2,dp);
-        return dp[n];
-    }
+    //     dp[n]= fibonacci(n-1,dp)+fibonacci(n-2,dp);
+    //     return dp[n];
+    // }
+     int tabulation(int n){
+        vector<int>v(n+2);
+        v[0]=0;
+        v[1]=1;
+        for(int i =2;i<=n;i++){
+            v[i]=v[i-1]+v[i-2];
+        }
+        return v[n];
+     }
+
     int fib(int n) {
         // return fibonacci(n);
-        vector<int>dp(n+1,-1);
-        return fibonacci(n,dp);
+        // vector<int>dp(n+1,-1);
+        // return fibonacci(n,dp);
+        return tabulation(n);
         
     }
 };
